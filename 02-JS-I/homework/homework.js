18795db4f38b5a96e3ea6d39a41bfb97fcb7ac24
Gustavo <eltavoadbman@gmail.com>
2022-07-20 21:39:34 -0300

    sptima

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -64,17 +64,20 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (x == y) {
+  if x === y {
+
     return true
   }
-  return false
+   else {
+    return false
+   }
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+   
   
 }
 
