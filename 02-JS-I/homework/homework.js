40e0965c87d2fa79da4ef83cfc51584367a0f2f7
Gustavo <eltavoadbman@gmail.com>
2022-07-22 00:10:50 -0300

    viernes 22

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,10 +1,10 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = '50';
+const nuevaString = 'hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 'hola';
+const nuevoNum = 50;
 
 // Crea una variable booleana:
 const nuevoBool = true;
