8a351d3c9d0e586daa844196a9cee3c02cfd9956
Gustavo <eltavoadbman@gmail.com>
2022-07-22 02:01:09 -0300

    g

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -148,7 +148,7 @@ function elevarAlCuadrado(num) {
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  
+  return num * num * num
 }
 
 function elevar(num, exponent) {
