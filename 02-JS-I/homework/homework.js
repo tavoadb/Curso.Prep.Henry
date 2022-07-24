ddc7c90a0e3b64a48fe66b205d0b06b8023dcb4b
Gustavo <eltavoadbman@gmail.com>
2022-07-24 00:52:21 -0300

    h

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -141,7 +141,7 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-   return num * num ;
+   return num * nu ;
   
 }
 
