75214a01fcc5fcac35ef5ce9723e92fe2c5e5a04
Gustavo <eltavoadbman@gmail.com>
2022-07-24 23:15:15 -0300

    dfd

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -207,6 +207,7 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return str + '!'
 }
 
 function combinarNombres(nombre, apellido) {
