5534e606fbdf86d39ddda947bdb1a019a41b7df5
Gustavo <eltavoadbman@gmail.com>
2022-07-26 11:58:48 -0300

    dfd

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -22,6 +22,12 @@ function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad > 18) {
+    return 'Allowed'
+  }
+  else if (edad < 18) {
+    return 'Not allowed'
+  }
 }
   
 function conection(status) {
