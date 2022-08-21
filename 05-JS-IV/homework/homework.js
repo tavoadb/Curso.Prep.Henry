00adebdb75d18b1d793f56b6d798008f33d46e6e
Gustavo <eltavoadbman@gmail.com>
2022-08-21 01:24:43 -0300

    21/08/22

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -93,12 +93,18 @@ function verificarPassword(usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
+  if (usuario.password === password) {
+    return true
+  }
+  return false
 }
 
 function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
+   usuario.password = nuevaPassword;
+   return usuario;
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
@@ -106,6 +112,8 @@ function agregarAmigo(usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // // Tu código:
+  usuario.amigos.push(nuevoAmigo)
+  return usuario
 }
 
 function pasarUsuarioAPremium(usuarios) {
@@ -114,6 +122,11 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  for (var i = 0 ; i < usuarios.length ; i++){
+    usuarios[i].esPremium = true ;
+    }
+    
+  return usuarios;
 }
 
 function sumarLikesDeUsuario(usuario) {
@@ -123,6 +136,12 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  var suma = 0
+  for(var i = 0 ; i < usuario.posts.length ; i++){
+    suma = usuario.posts[i].likes + suma 
+  }
+  return suma 
+
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -135,7 +154,19 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+  var productoo = 0
+  var descuento = 0
 
+  
+
+    producto.calcularPrecioDescuento = function(){
+      productoo = producto.precio * producto.porcentajeDeDescuento;
+      descuento = producto.precio - productoo;
+      return descuento 
+    
+  
+  }
+return producto
 }
 
 // No modificar nada debajo de esta línea
